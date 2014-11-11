8f7f712276081bbe424d760f2c4ede9f3c0ff05e
David Kraft-Ishihama <david.kraft-ishihama@blackbaud.com>
2014-11-11 12:22:23 -0600

    sqlh2

--- src/main/groovy/com/dojo/rest/GreetingController.groovy
+++ src/main/groovy/com/dojo/rest/GreetingController.groovy
@@ -1,11 +1,16 @@
+import groovy.sql.Sql
 import org.springframework.web.bind.annotation.*
 import com.dojo.rest.*
 
 @RestController
 public class GreetingController {
+    static instance = Sql.newInstance('jdbc:h2:tcp://localhost/~/test', "sa", null, "org.h2.Driver")
+    static id = 0;
 
     @RequestMapping("/save")
     public Child greeting(@RequestParam(value="childName", required=true) String childName) {
+        instance.execute("Insert into greeting values (?, ?);", id, childName)
+        id++
         return new Child(name : childName)
     }
 }
